91e03d30e72a51f2fe9d68dbfeef08943d7241fb
Maueeza Bhatkar <90929270+maueeza@users.noreply.github.com>
2023-05-21 19:40:16 +0530

    refactor : added products
    Added products

--- e-commerce/src/backend/db/products.js
+++ e-commerce/src/backend/db/products.js
@@ -6,25 +6,41 @@ import { v4 as uuid } from "uuid";
  * */
 
 export const products = [
-  {
-    _id: uuid(),
-    title: "You Can WIN",
-    author: "Shiv Khera",
-    price: "5000",
-    categoryName: "non-fiction",
-  },
-  {
-    _id: uuid(),
-    title: "You are Winner",
-    author: "Junaid Qureshi",
-    price: "3000",
-    categoryName: "horror",
-  },
-  {
-    _id: uuid(),
-    title: "Think and Grow Rich",
-    author: "Shiv Khera",
-    price: "1000",
-    categoryName: "fiction",
-  },
+  //categories for home page
+	{
+		id: uuid(),
+		title: "The Ethnic Image",
+		author: "Maueeza",
+		price: 599,
+		categoryName: "Categories",
+		imgSrc: "https://g3fashion.com/blog/wp-content/uploads/2021/06/ethnic-wear-e1624093120626.jpg",
+		type: "ETHNIC WEAR"
+	},
+	{
+		id: uuid(),
+		title: "The Western Image",
+		author: "Maueeza",
+		price: 699,
+		categoryName: "Categories",
+		imgSrc: "https://images.meesho.com/images/products/46055179/6appp_512.webp",
+		type: "WESTERN WEAR"
+	},
+	{
+		id: uuid(),
+		title: "The Sleepwear Image",
+		author: "Maueeza",
+		price: 449,
+		categoryName: "Categories",
+		imgSrc: "https://m.media-amazon.com/images/I/71dVOLsuJeS._UY550_.jpg",
+		type: "SLEEP WEAR"
+	},
+	{
+		id: uuid(),
+		title: "The Casual-wear Image",
+		author: "Maueeza",
+		price: 550,
+		categoryName: "Categories",
+		imgSrc: "https://images.unsplash.com/photo-1618554754947-e01d5ce3c549?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2FzdWFsJTIwd29tYW58ZW58MHx8MHx8&w=1000&q=80",
+		type: "CASUAL WEAR"
+	}
 ];
